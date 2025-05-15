196d813a44e949ac5f4ec66a798f67a40853f691
Priya Anand <priiyaaa1102@gmail.com>
2025-05-16 00:26:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const apiKey = "pub_87314e4938dd3ea6c326c32d5bb1349afbf2e";
-const url = "https://newsdata.io/api/1/news?apikey=pub_87314e4938dd3ea6c326c32d5bb1349afbf2e&q=news&country=in&language=en,hi&category=health,politics,science,sports,technology ";
+const apiKey = "63e2e2243a5f4357a2822e1e16abb456";
+const url = "https://newsapi.org/v2/everything?q=";
 
 window.addEventListener("load", () => fetchNews("india"));
 
